2f4757089435c4b8a73ff9b1ff03787ebdfcd4e2
Himanshi Jangid <himanshijangid444@gmail.com>
2023-07-30 12:14:44 +0530

    fvfgg

--- day 02/react-app/src/App.js
+++ day 02/react-app/src/App.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./components/ExpenseItem";
+import Expenses from './components/Expenses';
 
 const expenses = [
   {id : 'e1' ,title : 'Rice' , amount : 100, date : new Date(2023,7,26)},
@@ -17,26 +18,7 @@ function App() {
   return (
     <div>
     <h1>Let's get started</h1>
-    <ExpenseItem 
-    title = {expenses[0].title}
-    amount = {expenses[0].amount}
-    date = {expenses[0].date}
-    ></ExpenseItem>
-    <ExpenseItem 
-    title = {expenses[1].title}
-    amount = {expenses[1].amount}
-    date = {expenses[1].date}
-    ></ExpenseItem>
-    <ExpenseItem 
-    title = {expenses[2].title}
-    amount = {expenses[2].amount}
-    date = {expenses[2].date}
-    ></ExpenseItem>
-    <ExpenseItem 
-    title = {expenses[3].title}
-    amount = {expenses[3].amount}
-    date = {expenses[3].date}
-    ></ExpenseItem>
+    <Expenses items = {expenses} />
    
 
     </div>
